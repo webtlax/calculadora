7a9ea43352583c0a86b9fa31e6f453b38e4a39b8
GreBJ <47370977+GreBJ@users.noreply.github.com>
2019-03-08 12:51:14 -0600

    Add files via upload

---
+++ Calculadora/Multiplicacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Multiplicacion
+    {
+        int multiplicar = 0;
+
+        public Multiplicacion (int n1, int n2)
+        {
+            this.multiplicar = n1 * n2;
+        }
+
+        public string ToString()
+        {
+            return "El resultado de tu suma es " + multiplicar;
+        }
+    }
+}
