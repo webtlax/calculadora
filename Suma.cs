115c362a361a65b403de7f3713c5d36df99055c3
fil8500 <48359649+fil8500@users.noreply.github.com>
2019-03-08 18:42:44 +0000

    Add files via upload

---
+++ Suma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Suma
+    {
+        int sumar = 0;
+
+        public Suma(int n1, int n2) {
+            this.sumar = n1 + n2;
+        }
+
+        public string ToString() {
+            return "El resultado de tu suma es " + sumar;
+        }
+    }
+}
