16340d31410f24cc39eaa5bafd632d27b5b70f5a
Oriph <48359399+Oriph@users.noreply.github.com>
2019-03-08 12:50:05 -0600

    Add files via upload

---
+++ Resta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Resta
+    {
+        int restar = 0;
+
+        public Resta(int r1, int r2)
+        {
+            this.restar = r1 + r2;
+        }
+
+        public string ToString()
+        {
+            return "El resultado es " + restar;
+        }
+    }
+}
