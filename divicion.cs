1c796a8ab75115526207e6e9ee00db0bfabfe35b
felipop <43798043+felipop@users.noreply.github.com>
2019-03-08 19:43:06 +0100

    Divicion_felipop

---
+++ divicion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculdora
+{
+    class Divicion
+    {
+        int x;
+        int y;
+        int Result;
+
+        public Divicion () {
+            Console.WriteLine("Dame tu primer numero:");
+                x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dame tu segundo numero:");
+            y = int.Parse(Console.ReadLine());
+            Result = x / y;
+            Console.WriteLine("el resultado de la divicion es {0}", Result);
+
+        }
+
+       
+        
+       
+    }
+}
