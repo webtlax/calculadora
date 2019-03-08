76355d3e4029feb945e020714d99371651f08ae2
felipop <43798043+felipop@users.noreply.github.com>
2019-03-08 20:36:18 +0100

    Division_Corregida.
    Division_Corregida

---
+++ Division.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculdora
+{
+    class Division
+    {
+            int Divisionn = 0;
+
+            public Division(int n1, int n2)
+            {
+                this.Divisionn = n1 / n2;
+            }
+
+            public string ToString()
+            {
+                return "El resultado de la Divicion es:  "+ Divisionn;
+
+            }
+
+       
+        
+       
+    }
+}
